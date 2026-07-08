014921426dfeaec0321e1753c29be9b8fa74b83f
slvedrd1 <slvedrd1@gmail.com>
2026-07-08 12:41:39 -0400

    intento 6

---
+++ intento6.py
@@ -0,0 +1,304 @@
+#---------------------- Funcion menu y leer opcion --------------------
+def menu():
+
+    print("========== MENÚ PRINCIPAL ==========")
+    print("1. Unidades por tipo de arreglo")
+    print("2. Búsqueda de arreglos por rango de precio")
+    print("3. Actualizar precio de arreglo")
+    print("4. Agregar arreglo")
+    print("5. Eliminar arreglo")
+    print("6. Salir")
+    print("=====================================")
+
+def leer_opcion():
+
+    while True:
+        try:
+            opcion = int(input("Ingrese una opcion del menu: "))
+
+            if opcion > 0 and opcion <= 6:
+                return opcion
+            else:
+                print("Debe seleccionar una opcion valida")
+        except ValueError:
+            print("Debe seleccionar una opcion valida")
+
+#-------------------- OPCIONES 1 , 2, 3 --------------------------
+def unidades_tipo(tipo, dicc_arreglos, dicc_bodega):
+
+    cantdispo = 0
+
+    for codigo, datos in dicc_arreglos.items():
+
+        if tipo == datos[1]:
+            cantdispo += dicc_bodega[codigo][1]
+
+    print(f"--- Unidades por tipo {tipo} ---")
+    print(f"El total de unidades disponibles es: {cantdispo}")
+    print("")
+
+def busqueda_precio(p_min, p_max, dicc_arreglos, dicc_bodega):
+
+    lista = []
+
+    for codigo, datos in dicc_bodega.items():
+
+        if datos[0] >= p_min and datos[0] <= p_max and datos[1] > 0:
+            nombre = dicc_arreglos[codigo][0]
+            lista.append(f"{nombre} -- {codigo}")
+
+    if len(lista) == 0:
+        print("No hay arreglos en ese rango de precios.")
+        print("")
+    else:
+        lista.sort()
+        print("--- ARREGLOS EN RANGO DE PRECIO ---")
+        for datos in lista:
+
+            print(f"{datos}")
+        print("")
+
+def buscar_codigo(codigo, dicc_arreglos):
+
+    return codigo in dicc_arreglos
+
+def actualizar_precio(codigo, nuevoprecio, dicc_arreglos, dicc_bodega):
+
+    if buscar_codigo(codigo, dicc_arreglos):
+        dicc_bodega[codigo][0] = nuevoprecio
+        return True
+    return False
+
+#-------------------- validaciones true o false, opcion 4 ------------------
+
+def validar_codigo(codigo, dicc_arreglos):
+
+    if codigo.strip() == "":
+        return False
+    if codigo in dicc_arreglos:
+        return False
+    return True
+
+def validar_texto(texto): #Sirve para ( nombre, tipo, color, temporada)
+
+    if texto.strip() == "":
+        return False
+    return True
+
+def validar_tamano(tamano):
+
+    return tamano.upper() in ["S", "M", "L"]
+
+def validar_tarjeta(tarjeta):
+
+    return tarjeta.lower() in ["s", "n"]
+
+def validar_precio(precio):
+
+    if precio > 0:
+        return True
+    return False
+
+def validar_unidades(unidades):
+
+    if unidades >= 0:
+        return True
+    return False
+
+def agregar_arreglo(codigo, nombre, tipo, color, tamano, tarjeta, temporada, precio, unidades, dicc_arreglos, dicc_bodega):
+
+    if codigo in dicc_arreglos:
+        return False
+    
+    dicc_arreglos[codigo] = [nombre, tipo, color, tamano, tarjeta, temporada]
+    dicc_bodega[codigo] = [precio, unidades]
+    return True
+
+def eliminar_arreglo(codigo, dicc_arreglos, dicc_bodega):
+
+    if buscar_codigo(codigo, dicc_arreglos):
+        del dicc_arreglos[codigo]
+        del dicc_bodega[codigo]
+        return True
+    return False
+
+
+arreglos = {
+    'FLO1': ['Ramo Primavera', 'ramo', 'rosado', 'M', True,'primavera'],
+    'FLO2': ['Caja Elegante', 'caja', 'blanco', 'L', True, 'todo año'],
+    'FLO3': ['Ramo Solar', 'ramo', 'amarillo', 'S', False, 'verano'],
+    'FLO4': ['Centro Mesa', 'centro', 'rojo', 'M', True, 'todo año'],
+    'FLO5': ['Ramo Bosque', 'ramo', 'verde', 'L', False, 'otoño'],
+    'FLO6': ['Caja Noche', 'caja', 'morado', 'M', True, 'invierno'],
+}
+
+bodega = {
+    'FLO1': [15990, 8],
+    'FLO2': [29990, 3],
+    'FLO3': [9990, 12],
+    'FLO4': [24990, 5],
+    'FLO5': [19990, 0],
+    'FLO6': [22990, 6],
+}
+
+while True:
+
+    menu()
+    opcionMenu = leer_opcion()
+
+    match opcionMenu:
+
+        case 1:
+            buscartipo = input("Ingrese tipo de arreglo: ").strip().lower()
+            unidades_tipo(buscartipo, arreglos, bodega)
+
+        case 2:
+            while True:
+                try:
+                    precioMin = int(input("Ingrese precio minimo: "))
+                    precioMax = int(input("Ingrese precio maximo: "))
+
+                    if precioMin >= 0 and precioMax >= precioMin:
+                        break
+                    else:
+                        print("ERROR: debe ingresar valores enteros.")
+                except ValueError:
+                    print("ERROR: debe ingresar valores enteros")
+
+            busqueda_precio(precioMin, precioMax, arreglos, bodega)
+
+        case 3:
+            while True:
+                codigoArreglo = input("Ingrese el codigo del arreglo: ").strip().upper()
+
+                while True:
+                    try:
+                        nuevPrecio = int(input("Ingrese nuevo precio: "))
+
+                        if nuevPrecio > 0:
+                            break
+                        else:
+                            print("ERROR: debe ser entero positivo.")
+                    except ValueError:
+                        print("ERROR: debe ser entero positivo.")
+
+                if actualizar_precio(codigoArreglo, nuevPrecio, arreglos, bodega):
+                    print("--- PRECIO ACTUALIZADO ---")
+                    print("")
+                else:
+                    print("--- EL CODIGO NO EXISTE ---")
+
+                while True:
+                    seguir = input("Desea actualizar otro precio (s/n): ").strip().lower()
+
+                    if seguir == "s":
+                        break
+                    if seguir == "n":
+                        break
+                    else:
+                        print("ERROR: debe ser (s/n).")
+
+                if seguir == "n":
+                    break
+            
+        case 4:
+
+            while True:
+                codigo = input("Ingrese codigo del arreglo: ").strip().upper()
+
+                if validar_codigo(codigo, arreglos):#recordar siempre que lleva 2 parametros
+                    break
+                else:
+                    print("ERROR: no debe estar repetido ni quedar en blanco.")
+
+            while True:
+                nombre = input("Ingrese nombre: ").strip().title()
+
+                if validar_texto(nombre):
+                    break
+                else:
+                    print("ERROR: no debe estar vacio ni solo espacios en blanco.")
+
+            while True:
+                tipo = input("Ingrese tipo: ").strip().lower()
+
+                if validar_texto(tipo):
+                    break
+                else:
+                    print("ERROR: no debe estar vacio ni solo espacios en blanco.")
+
+            while True:
+                color = input("Ingrese color: ").strip().lower()
+
+                if validar_texto(color):
+                    break
+                else:
+                    print("ERROR: no debe estar vacio ni solo espacios en blanco.")
+
+            while True:
+                tamano = input("Ingrese tamaño (S-M-L): ").strip().upper()
+
+                if validar_tamano(tamano):
+                    break
+                else:
+                    print("ERROR: debe ser exactamente (S-M-L).")
+
+            while True:
+                tarjeta = input("Ingrese tarjeta (s-n): ").strip().lower()
+
+                if validar_tarjeta(tarjeta):
+                    tarjetabool = True if tarjeta == "s" else False #cambiamos la variable a bool
+                    break
+                else:
+                    print("ERROR: debe ser (s-n).")
+
+            while True:
+                temporada = input("Ingrese temporada: ").strip().lower()
+
+                if validar_texto(temporada):
+                    break
+                else:
+                    print("ERROR: no debe estar vacio ni solo espacios en blanco.")
+
+            while True:
+                try:
+                    precio = int(input("Ingrese precio: ").strip())
+
+                    if validar_precio(precio):
+                        break
+                    else:
+                        print("ERROR: debe ser un numero entero mayor que 0.")
+                except  ValueError:
+                    print("ERROR: debe ser un numero entero mayor que 0.")
+
+            while True:
+                try:
+                    unidades = int(input("Ingrese unidades: ").strip())
+
+                    if validar_unidades(unidades):
+                        break
+                    else:
+                        print("ERROR: debe ser un numero entero mayor o igual a 0.")
+                except ValueError:
+                    print("ERROR: debe ser un numero entero mayor o igual a 0.")
+
+            if agregar_arreglo(codigo, nombre, tipo, color, tamano, tarjetabool, temporada, precio, unidades, arreglos, bodega):
+                print("--- ARREGLO AGREGADO EXITOSAMENTE ---")
+                print("")
+            else:
+                print("--- EL CODIGO YA EXISTE --- ")      
+                print("")
+
+        case 5:
+            eliminar = input("Ingrese el codigo que desea eliminar: ").strip().upper()
+
+            if eliminar_arreglo(eliminar, arreglos, bodega):
+                print("--- ARREGLO ELIMINADO ---")
+                print("")
+            else:
+                print("--- EL CODIGO NO EXISTE ---")
+                print("")
+
+        case 6:
+            print("--- PROGRAMA FINALIZADO... ---")
+            break
